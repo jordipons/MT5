5797a61e36879e006396e506455a8d102dd174f1
idrojsnop <idrojsnop@gmail.com>
2015-03-30 07:48:11 +0200

    Test nearly finished where one can train and test. One can push save and next, jumping from one song to other.

--- client/js/sound.js
+++ client/js/sound.js
@@ -388,6 +388,7 @@ function loadSong(songName) {
         loadAllSoundSamples();
     };
     xhr.send();
+    //return song.instruments.length
 }
 
 function getMousePos(canvas, evt) {
@@ -511,15 +512,18 @@ function showWelcomeMessage() {
     View.frontCanvasContext.save();
     View.frontCanvasContext.font = '14pt Arial';
     View.frontCanvasContext.fillStyle = 'white';
-    View.frontCanvasContext.fillText('Second, put your name in the ID box and then', 50, 200);
-    View.frontCanvasContext.fillText('choose an experiment in the drop down menu! ', 50, 220);
-    View.frontCanvasContext.fillText('First: read the task that ', 315, 100);
-    View.frontCanvasContext.fillText('is defined in red at the black box! ', 315, 120);
+    View.frontCanvasContext.fillText('1) Choose an experiment: TRAIN or START TEST!', 50, 200);
+    View.frontCanvasContext.fillText('Other options are for individual evaluation of the test songs.', 50, 225);
+    View.frontCanvasContext.fillText('2) Once the best mix is found, click Save & Next to continue.', 120, 100);
+    //View.frontCanvasContext.fillText('1) Read the task  written in red  ', 315, 100);
+    //View.frontCanvasContext.fillText('at the black box! ', 315, 120);
 
     // Draws an arrow in direction of the drop down menu
     // x1, y1, x2, y2, width of arrow, color
-    drawArrow(View.frontCanvasContext, 180, 170, 10, 10, 10, 'lightGreen');
-    drawArrow(View.frontCanvasContext, 450, 80, 450, 10, 10, 'lightGreen');
+    drawArrow(View.frontCanvasContext, 160, 170, 10, 10, 10, 'lightGreen');
+    drawArrow(View.frontCanvasContext, 300, 70, 250, 10, 10, 'lightGreen');
+    //drawArrow(View.frontCanvasContext, 450, 80, 450, 10, 10, 'lightGreen');
+
 
     View.frontCanvasContext.restore();
 }
